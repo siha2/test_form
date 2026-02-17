f6efdd435569d65f5f6bd88ce8a62300cdc1eee6
siha2 <sihaemad55@gmail.com>
2026-02-17 21:57:44 +0200

    make it fit for vercel

--- api/index.php
+++ api/index.php
@@ -21,7 +21,7 @@
         // If No Errors Send The Email [ mail(To, Subject, Message, Headers, Parameters) ]
         
         $headers = 'From: ' . $mail . '\r\n';
-        $myEmail = 'sihaemad555@gmail.com';
+        $myEmail = getenv('MY_EMAIL');
         $subject = 'Contact Form';
         
         if (empty($formErrors)) {
@@ -46,7 +46,8 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Elzero Contact Form</title>
+        <title>Contact Form</title>
+        <link rel="icon" href="/me.webp" type="image/x-icon" />
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/font-awesome.min.css" />
         <link rel="stylesheet" href="css/contact.css" />
